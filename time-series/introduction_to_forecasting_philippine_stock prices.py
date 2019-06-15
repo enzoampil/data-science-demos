9a8cfc52326024f77cc34919cb03c3ad34313f23
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2019-06-16 01:47:28 +0800

    add additional evaluation section

--- time-series/introduction_to_forecasting_philippine_stock prices.py
+++ time-series/introduction_to_forecasting_philippine_stock prices.py
@@ -27,7 +27,9 @@ ts = jfc[['CHART_DATE', 'CLOSE']]
 ts.columns = ['ds', 'y']
 ts.head()
 
-m = Prophet(daily_seasonality=True, yearly_seasonality=True).fit(ts)
+HOLDOUT_START = '2019-03-01'
+
+m = Prophet(daily_seasonality=True, yearly_seasonality=True).fit(ts[ts.ds < HOLDOUT_START])
 future = m.make_future_dataframe(periods=7*4*12, freq='D')
 
 pred = m.predict(future)
@@ -35,6 +37,40 @@ pred = m.predict(future)
 from matplotlib import pyplot as plt
 fig1 = m.plot(pred)
 plt.title('Jollibee: Forecasted Daily Closing Price', fontsize=25)
+
+pred_holdout = pred[(pred.ds >= HOLDOUT_START)&(pred.ds <= ts.ds.max())].set_index('ds').yhat
+target_holdout = ts[ts.ds >= HOLDOUT_START].set_index('ds')
+comb = pd.concat([pred_holdout, target_holdout], axis=1).dropna()
+comb
+
+import numpy as np
+rmse_holdout = np.sqrt(comb.yhat.subtract(comb.y).pow(2).mean())
+rmse_holdout
+
+mae_holdout = np.mean(np.abs(comb.yhat.subtract(comb.y)))
+mae_holdout
+
+comb.columns = ['Predicted', 'Actual']
+comb.plot(figsize=(15, 10))
+plt.title('Predicted vs Actual JFC Closing Price \n Validation Set RMSE: {}'.format(round(rmse_holdout, 2)), fontsize=25)
+
+# Advanced diagnostics
+from fbprophet.diagnostics import cross_validation
+df_cv = cross_validation(m, initial=f'{ts.shape[0] - 30} days', horizon = '30 days')
+
+from fbprophet.diagnostics import performance_metrics
+df_p = performance_metrics(df_cv)
+df_p['horizon_days'] = df_p.horizon.dt.days
+df_p.set_index('horizon_days').mae.plot()
+
+# Assess accuracy
+import numpy as np
+
+def mean_absolute_percentage_error(y_true, y_pred): 
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+mean_absolute_percentage_error(y_true, y_pred)
 
 #Plotting returns of JFC
 ts_pct = ts.set_index('ds').pct_change()
